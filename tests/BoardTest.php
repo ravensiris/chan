66f2cda240e624fbaef36fba1eebff5273239102
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-06 15:00:50 +0100

    Make tests

--- tests/BoardTest.php
+++ tests/BoardTest.php
@@ -8,16 +8,45 @@ class BoardTest extends TestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBoardsList()
+    public function testList()
     {
-        $boards = Board::factory()->count(3)->make();
+        $boards = Board::factory()->count(3)->create();
 
         $response = $this->get('/boards/');
-        $response->assertJson($boards);
+        $response->assertResponseOk();
+        $response->seeJsonEquals($boards->toArray());
+    }
+
+    public function testShow()
+    {
+        $board = Board::factory()->create();
+
+        $response = $this->get("/boards/{$board->id}");
+        $response->assertResponseOk();
+        $response->seeJsonEquals($board->toArray());
+    }
+
+    public function testShowInvalidUuid()
+    {
+        // TODO: After creating the error handler check if it makes sense
+        // to use such convoluted way of expressing errors
+        $error_data = ['error' => [
+            'errors' =>
+            [
+                [
+                    'domain' => 'board',
+                    'reason' => 'invalidUuid',
+                    'message' => '`123` is not a valid UUIDv4.',
+                    'locationType' => 'path',
+                    'location' => '/boards/'
+                ]
+            ],
+            'code' => 400,
+            'message' => '`123` is not a valid UUIDv4.'
+        ]];
+
+        $response = $this->get("/boards/123");
+        $response->assertResponseStatus(400);
+        $response->seeJsonEquals($error_data);
     }
 }
