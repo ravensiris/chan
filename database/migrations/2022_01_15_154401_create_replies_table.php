4974c465af40e0b678006fdda5367b798bf92d24
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-16 02:17:31 +0100

    Replies migration

---
+++ database/migrations/2022_01_15_154401_create_replies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Thread;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('thread_id')->foreign(Thread::class);
+            $table->uuid('image_id')->nullable();
+            $table->string('title', 50);
+            $table->string('body', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
