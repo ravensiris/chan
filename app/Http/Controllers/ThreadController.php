64ba1a49c07df2cd805ad7969c55677a7670b5c2
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-16 02:20:42 +0100

    create thread create method

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Thread;
 use App\Models\Board;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 class ThreadController extends Controller
 {
@@ -21,6 +22,50 @@ class ThreadController extends Controller
         if ($thread->board_id !== $board_uuid) {
             throw new ModelNotFoundException();
         }
+        return $thread;
+    }
+
+    public function create(Request $request, $board_uuid)
+    {
+        $board = Board::findOrFailUuid($board_uuid);
+
+        try {
+            $this->validate($request, [
+                'title' => 'string|required|between:1,50',
+                'body' => 'string|required|between:1,500',
+                'image' => 'boolean'
+            ]);
+        } catch (\Throwable $e) {
+            // TODO: extend validator
+            // HACK: fixed response
+            return response()->json(
+                [
+                    'error' => [
+                        'errors' => [
+                            'domain' => 'thread',
+                            'reason' => 'validationFailureGeneric',
+                            'locationType' => 'json',
+                            'location' => 'unknown'
+                        ],
+                        'code' => 400,
+                        'message' => 'Validation error.'
+                    ]
+                ],
+                400
+            );
+        }
+
+        $data = $request->all();
+        if ($data['image'] ?? false) {
+            // TODO: Return uuid pointing to an image table
+            // TODO: User can post image to that uuid
+        }
+
+        $thread = $board->threads()->create();
+        $thread->replies()->create($request->only(['title', 'body']));
+        $thread->refresh();
+        $thread->op;
+
         return $thread;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@
 $router->group(['prefix' => 'boards'], function () use ($router) {
     $router->get('/', 'BoardController@list');
     $router->get('/{board_uuid}/threads', 'ThreadController@list');
+    $router->post('/{board_uuid}/threads', 'ThreadController@create');
     $router->get('/{board_uuid}/threads/{uuid}', 'ThreadController@show');
     $router->get('/{uuid}', 'BoardController@show');
 });
