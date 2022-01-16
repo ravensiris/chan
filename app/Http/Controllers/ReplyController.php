3b5aa9ef147406578c247b888a35c49f21fe5254
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-01-16 22:07:58 +0100

    Add create and list methods in ReplyController

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reply;
+use App\Models\Thread;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
@@ -11,5 +14,56 @@ class ReplyController extends Controller
     public function show($uuid)
     {
         return Reply::findOrFailUuid($uuid);
+    }
+
+    public function list($thread_uuid)
+    {
+        $thread = Thread::findOrFailUuid($thread_uuid);
+        return $thread->replies;
+    }
+
+    public function create(Request $request, $board_uuid, $thread_uuid)
+    {
+        $thread = Thread::findOrFailUuid($thread_uuid);
+
+        if ($thread->board_id !== $board_uuid) {
+            throw new ModelNotFoundException();
+        }
+
+        try {
+            $this->validate($request, [
+                'title' => 'string|required|between:1,50',
+                'body' => 'string|required|between:1,500',
+                'image' => 'boolean'
+            ]);
+        } catch (\Throwable $e) {
+            // TODO: extend validator
+            // HACK: fixed response
+            return response()->json(
+                [
+                    'error' => [
+                        'errors' => [
+                            'domain' => 'thread',
+                            'reason' => 'validationFailureGeneric',
+                            'locationType' => 'json',
+                            'location' => 'unknown'
+                        ],
+                        'code' => 400,
+                        'message' => 'Validation error.'
+                    ]
+                ],
+                400
+            );
+        }
+
+        $data = $request->all();
+        if ($data['image'] ?? false) {
+            // TODO: Return uuid pointing to an image table
+            // TODO: User can post image to that uuid
+        }
+
+        $reply = $thread->replies()->create($request->only(['title', 'body']));
+
+        return $reply;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ $router->group(['prefix' => 'boards'], function () use ($router) {
     $router->get('/{board_uuid}/threads', 'ThreadController@list');
     $router->post('/{board_uuid}/threads', 'ThreadController@create');
     $router->get('/{board_uuid}/threads/{uuid}', 'ThreadController@show');
+    // TODO: this is getting ridiculous. Find a better way. I beg of you
+    $router->get('/{board_uuid}/threads/{thread_uuid}/replies', 'ReplyController@list');
+    $router->post('/{board_uuid}/threads/{thread_uuid}/replies', 'ReplyController@create');
+    $router->get('/{board_uuid}/threads/{thread_uuid}/replies/{uuid}', 'ReplyController@show');
     $router->get('/{uuid}', 'BoardController@show');
 });
